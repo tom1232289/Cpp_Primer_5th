13640f9a591dae95c069bc66e0a3c4031e3a8a70
tom1232289 <47622683+tom1232289@users.noreply.github.com>
2019-02-19 11:19:40 +0800

    Add files via upload

--- practice10.24/practice10.24/源10.24.cpp
+++ practice10.24/practice10.24/源10.24.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+ï»¿#include<iostream>
 #include<vector>
 #include<string>
 #include<functional>
@@ -10,13 +10,13 @@ using namespace placeholders;
 bool check_size(const string &s, size_t sz) {
 	return s.size() <= sz;
 }
-//
+
 int main()
 {
 	vector<int> ivec = { 0,1,2,3,4,5,6,7,8,9 };
 	string s = "abc";
 	auto p = find_if(ivec.begin(), ivec.end(), bind(check_size, s, _1));
-	cout << "µÚ" << p - ivec.begin() + 1 << "¸öÊý" << *p << ">=" << s << "µÄ³¤¶È" << endl;
+	cout << "ç¬¬" << p - ivec.begin() + 1 << "ä¸ªæ•°" << *p << ">=" << s << "çš„é•¿åº¦" << endl;
 
 	system("pause"); return 0;
 }
